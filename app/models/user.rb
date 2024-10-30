d13d4147bbe45388d87d89e8eea270ad243dc4e9
prokoninaritai <s0a7c5h1i@gmail.com>
2024-10-31 05:31:09 +0900

    usersテーブルマイグレーションファイル、モデルの制約

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヴ]+\z/, message: '全角カナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+end
