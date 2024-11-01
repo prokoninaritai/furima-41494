8c5ac6285edc500abf5fa00d5180878212aa88c5
prokoninaritai <s0a7c5h1i@gmail.com>
2024-11-01 21:33:06 +0900

    ユーザーモデル制約修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヴ]+\z/, message: '全角カナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナを使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
