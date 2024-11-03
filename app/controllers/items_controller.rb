c0a77bf7240a8f0cc0fb86c8d860d00dd527474b
prokoninaritai <s0a7c5h1i@gmail.com>
2024-11-03 14:37:09 +0900

    商品出品ページを表示できるように実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+    @categories = Category.all
+    @conditions = Condition.all
+    @prefectures = Prefecture.all
+    @delivery_charges = Delivery_charge.all
+    @delivery_days = Delivery_day.all
+  end
+
 end
