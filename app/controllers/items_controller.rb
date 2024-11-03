a1d58179e4a939b28aa3b7786f4965af39d291f0
prokoninaritai <s0a7c5h1i@gmail.com>
2024-11-03 15:49:29 +0900

    商品をDBに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,29 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @categories = Category.all
-    @conditions = Condition.all
-    @prefectures = Prefecture.all
-    @delivery_charges = Delivery_charge.all
-    @delivery_days = Delivery_day.all
+    set_item_dependencies
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      set_item_dependencies
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+    private
+    def item_params
+      params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price ).merge(user_id:current_user.id)
+    end
+
+    def set_item_dependencies
+      @categories = Category.all
+      @conditions = Condition.all
+      @prefectures = Prefecture.all
+      @delivery_charges = DeliveryCharge.all
+      @delivery_days = DeliveryDay.all
+    end
 end
