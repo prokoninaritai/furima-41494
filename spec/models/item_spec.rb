280360571754e823d7e933ebb0b6bc6e078a8ef3
prokoninaritai <s0a7c5h1i@gmail.com>
2024-11-04 16:29:48 +0900

    Itemモデル単体テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,119 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品が保存できる場合' do
+      it 'name、description、category_id、condition_id、delivery_charge_id'\
+         'prefecture_id、delivery_day_id、price、image、userが存在していれば保存できる' do
+        expect(@item).to be_valid
+      end  
+    end
+
+    context '商品が保存できない場合' do
+      it 'imageは空では保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userは空では保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      it 'nameは空では保存できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionは空では保存できない' do
+        @item.description=''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'category_idは空では保存できない' do
+        @item.category_id=''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'category_idは1では保存できない' do
+        @item.category_id=1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idは空では保存できない' do
+        @item.condition_id=''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'condition_idは1では保存できない' do
+        @item.condition_id=1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'delivery_charge_idは空では保存できない' do
+        @item.delivery_charge_id=''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+      it 'delivery_charge_idは1では保存できない' do
+        @item.delivery_charge_id=1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+      it 'prefecture_idは空では保存できない' do
+        @item.prefecture_id=''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'prefecture_idは1では保存できない' do
+        @item.prefecture_id=1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'delivery_day_idは空では保存できない' do
+        @item.delivery_day_id=''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+      it 'delivery_day_idは1では保存できない' do
+        @item.delivery_day_id=1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+      it 'priceは空では保存できない' do
+        @item.price=''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceは299円以下では保存できない' do
+        @item.price=299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceは10,000,000円以上では保存できない' do
+        @item.price=10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it 'priceは全角数字では保存できない' do
+        @item.price='５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceは英字では保存できない' do
+        @item.price='Three hundred'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceは全角文字では保存できない' do
+        @item.price='3百'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
