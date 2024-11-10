0facca05c8d08fb2118515ad805be22585e7a2d5
prokoninaritai <s0a7c5h1i@gmail.com>
2024-11-10 18:21:30 +0900

    アソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,12 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+  has_one :order
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :prefecture
   belongs_to :delivery_charge
   belongs_to :delivery_day
+
 end
