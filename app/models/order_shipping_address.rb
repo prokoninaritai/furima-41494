32228260e250996721d8c42466439971677e642e
prokoninaritai <s0a7c5h1i@gmail.com>
2024-11-14 21:38:41 +0900

    バリデーションの編集

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -8,7 +8,6 @@ class OrderShippingAddress
     validates :city
     validates :address
     validates :mobile, format: { with: /\A\d{10,11}\z/, message: 'must be 10 or 11 digits' }
-    validates :order_id
     validates :user_id
     validates :item_id
   end
