3e169085f21dd79b2982da7c05a33e1df8b1ab52
prokoninaritai <s0a7c5h1i@gmail.com>
2024-11-14 21:38:17 +0900

    購入情報のフォーム表示・保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,29 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @order_shippint_address = OrderShippingAddress.new
+    @order_shipping_address = OrderShippingAddress.new
   end
 
   def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.save
+      redirect_to '/'
+    else
+      puts @order_shipping_address.errors.full_messages 
+      render :index, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_shipping_address).permit(:post_code, :prefecture_id, :city, :address, :building, :mobile).merge(user_id: current_user.id, item_id: @item.id)
+  end
+  
 end
