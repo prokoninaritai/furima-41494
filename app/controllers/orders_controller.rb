74ea321b4dfdbf6221822b513e09ac25c7c1a57f
prokoninaritai <s0a7c5h1i@gmail.com>
2024-11-17 20:14:33 +0900

    クレジットカード購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,19 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_shipping_address = OrderShippingAddress.new
   end
 
   def create
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if @order_shipping_address.valid?
+      pay_item
       @order_shipping_address.save
       redirect_to '/'
     else
       puts @order_shipping_address.errors.full_messages 
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,7 +26,16 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_shipping_address).permit(:post_code, :prefecture_id, :city, :address, :building, :mobile).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_shipping_address).permit(:post_code, :prefecture_id, :city, :address, :building, :mobile).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: @order_shipping_address.token,
+        currency: 'jpy'   
+      )
   end
   
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,6 +1,6 @@
 class OrderShippingAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :address, :building, :mobile, :order_id, :user_id, :item_id
+  attr_accessor :token, :post_code, :prefecture_id, :city, :address, :building, :mobile, :order_id, :user_id, :item_id
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,6 +10,7 @@ class OrderShippingAddress
     validates :mobile, format: { with: /\A\d{10,11}\z/, message: 'must be 10 or 11 digits' }
     validates :user_id
     validates :item_id
+    validates :token, presence: true
   end
  
   def save
